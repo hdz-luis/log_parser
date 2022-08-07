a2d4bb4125efbf72b2279d7d18a46ad08c30e93f
hdz-luis <86610746+hdz-luis@users.noreply.github.com>
2022-08-07 10:27:14 -0500

    Add files via upload

--- OutSystems Log Parser/bin/Debug/graphs/deviceinformation.py
+++ OutSystems Log Parser/bin/Debug/graphs/deviceinformation.py
@@ -1,6 +1,7 @@
 import os
 import re
 import codecs
+import traceback
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
@@ -9,53 +10,63 @@ myOsList = []
 myOsVersionList = []
 myOccurrencesList = []
 deviceInfoRegex = r"^([\w]+)\|([\d\.]+)\|([\d]+)\|.+"
+errorLogPath = os.getcwd() + "\\error_log.txt"
+
+def error_log(error_message):
+    with codecs.open(errorLogPath, "w", "utf-8", "ignore") as errorFile:
+        errorFile.writelines("Unexpected Error:\n" + str(error_message).strip())
 
 def create_graph(directoryPath, deviceInfoRegex):
-    foundFile = False
-    for root, subFolders, files in os.walk(directoryPath):
-        for f in files:
-            if "device_information" in f.lower():
-                foundFile = True
-                print("Creating the graph from the Device Information file")
-                absolutePathOfFile = os.path.join(root, f)
-                start = datetime.now()
-                with codecs.open(absolutePathOfFile, "r", "utf-8", "ignore") as tempFile:
-                    searchLines = tempFile.read()
-                    regex = re.compile(deviceInfoRegex, re.MULTILINE + re.IGNORECASE)
-                    for match in regex.finditer(searchLines):
-                        myOs = match.group(1)
-                        myOsVersion = match.group(2)
-                        myOccurrences = match.group(3)
+    try:
+        foundFile = False
+        for root, subFolders, files in os.walk(directoryPath):
+            for f in files:
+                if "device_information" in f.lower():
+                    foundFile = True
+                    print("Creating the graph from the Device Information file")
+                    absolutePathOfFile = os.path.join(root, f)
+                    start = datetime.now()
+                    with codecs.open(absolutePathOfFile, "r", "utf-8", "ignore") as tempFile:
+                        searchLines = tempFile.read()
+                        regex = re.compile(deviceInfoRegex, re.MULTILINE + re.IGNORECASE)
+                        for match in regex.finditer(searchLines):
+                            myOs = match.group(1)
+                            myOsVersion = match.group(2)
+                            myOccurrences = match.group(3)
 
-                        myOsList.append(myOs)
-                        myOsVersionList.append(myOsVersion)
-                        myOccurrencesList.append(int(myOccurrences))
+                            myOsList.append(myOs)
+                            myOsVersionList.append(myOsVersion)
+                            myOccurrencesList.append(int(myOccurrences))
 
-                plotdata = pd.DataFrame({"OS": myOsList, "OS_VERSION": myOsVersionList, "OCCURRENCES": myOccurrencesList})
-                fig_bar = px.bar(plotdata,
-                                 x="OS_VERSION",
-                                 y="OCCURRENCES",
-                                 hover_data=["OS", "OCCURRENCES"],
-                                 color="OS",
-                                 labels={"pop":"OCCURRENCES"},
-                                 height=500,
-                                 color_discrete_map={"Android": "green", "iOS": "goldenrod"},
-                                 title="Mobile OS: Their Versions And Their Occurrences")
-                fig_bar.update_layout(showlegend=False, title={"y":0.9, "x":0.5, "xanchor": "center", "yanchor": "top"})
-                fig_bar.update_traces(dict(marker_line_width=0))
-                fig_bar.show()
+                    plotdata = pd.DataFrame({"OS": myOsList, "OS_VERSION": myOsVersionList, "OCCURRENCES": myOccurrencesList})
+                    fig_bar = px.bar(plotdata,
+                                     x="OS_VERSION",
+                                     y="OCCURRENCES",
+                                     hover_data=["OS", "OCCURRENCES"],
+                                     color="OS",
+                                     labels={"pop":"OCCURRENCES"},
+                                     height=500,
+                                     color_discrete_map={"Android": "green", "iOS": "goldenrod"},
+                                     title="Mobile OS: Their Versions And Their Occurrences")
+                    fig_bar.update_layout(showlegend=False, title={"y":0.9, "x":0.5, "xanchor": "center", "yanchor": "top"})
+                    fig_bar.update_traces(dict(marker_line_width=0))
+                    fig_bar.show()
 
-                print("The graph from the Device Information file was created. Please check your browser.")
+                    print("The graph from the Device Information file was created. Please check your browser.")
 
-                del myOsList[:]
-                del myOsVersionList[:]
-                del myOccurrencesList[:]
+                    del myOsList[:]
+                    del myOsVersionList[:]
+                    del myOccurrencesList[:]
 
-                end = datetime.now()
-                print("\nElapsed time: {0}".format(end-start))
+                    end = datetime.now()
+                    print("\nElapsed time: {0}".format(end-start))
 
-    if not foundFile:
-        print("The Device Information file was not found in folder: \"" + directoryPath + "\"")
+        if not foundFile:
+            print("The Device Information file was not found in folder: \"" + directoryPath + "\"")
+    except Exception as e:
+        error_stack = traceback.format_exc()
+        error_log(error_stack)
+        print("A file has been created with the error message.\n\nUnexpected Error:\n", error_stack)
 
 directoryPath = input("In what folder is the Device Information file located? ")
 create_graph(directoryPath, deviceInfoRegex)
--- OutSystems Log Parser/bin/Debug/graphs/integrations.py
+++ OutSystems Log Parser/bin/Debug/graphs/integrations.py
@@ -1,73 +1,86 @@
 import os
 import plotly
+import codecs
+import traceback
 import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
 
+errorLogPath = os.getcwd() + "\\error_log.txt"
+
+def error_log(error_message):
+    with codecs.open(errorLogPath, "w", "utf-8", "ignore") as errorFile:
+        errorFile.writelines("Unexpected Error:\n" + str(error_message).strip())
+
 def create_graph(directoryPath):
-    foundLogs = False
-    for root, subFolders, files in os.walk(directoryPath):
-        for f in files:
-            if "integrationslog" in f.lower():
-                foundLogs = True
-                print("Creating graphs from the Integrations logs")
-                absolutePathOfFile = os.path.join(root, f)
-                start = datetime.now()
-                pd.options.mode.chained_assignment = None  # default="warn"
-                #PROCESSING THE EXCEL FILE
-                df_init = pd.read_excel(absolutePathOfFile,engine="openpyxl",usecols="B,C,F")
-                ##### PLOTTING Scatter Graph ######
-                print("Plotting the scatter graph")
-                #using plotly to create a scatter graph
-                fig_scatter= px.scatter(df_init, x="INSTANT", y="DURATION",color="ACTION",height=500,title="SLOW INTEGRATIONS ANALYSIS")
-                fig_scatter.update_layout(showlegend=False,title={       
-                        "y":0.9,
-                        "x":0.5,
-                        "xanchor": "center",
-                        "yanchor": "top"})
+    try:
+        foundLogs = False
+        for root, subFolders, files in os.walk(directoryPath):
+            for f in files:
+                if "integrationslog" in f.lower():
+                    foundLogs = True
+                    print("Creating graphs from the Integrations logs")
+                    absolutePathOfFile = os.path.join(root, f)
+                    start = datetime.now()
+                    pd.options.mode.chained_assignment = None  # default="warn"
+                    #PROCESSING THE EXCEL FILE
+                    df_init = pd.read_excel(absolutePathOfFile,engine="openpyxl",usecols="B,C,F")
+                    ##### PLOTTING Scatter Graph ######
+                    print("Plotting the scatter graph")
+                    #using plotly to create a scatter graph
+                    fig_scatter= px.scatter(df_init, x="INSTANT", y="DURATION",color="ACTION",height=500,title="SLOW INTEGRATIONS ANALYSIS")
+                    fig_scatter.update_layout(showlegend=False,title={       
+                            "y":0.9,
+                            "x":0.5,
+                            "xanchor": "center",
+                            "yanchor": "top"})
 
-                #intializing a new dataframe from the filtered one
-                df_dateonly = df_init
+                    #intializing a new dataframe from the filtered one
+                    df_dateonly = df_init
 
-                df_dateonly.assign(INSTANT=df_dateonly.INSTANT.dt.round("H"))
-                #removing the time and keeping only the date on the Instant column
-                df_dateonly["INSTANT"] = pd.to_datetime(df_dateonly["INSTANT"]).dt.date
-                #gouping by instant, Integrations, by the number of occurrences of integrations
-                new = df_dateonly.groupby(["INSTANT","ACTION"]).size()
-                #converting the above series type variable to a dataframe
-                df= new.to_frame().reset_index()
-                #changing the name of the count column to "count"
-                df = df.rename(columns= {0: "count"})
+                    df_dateonly.assign(INSTANT=df_dateonly.INSTANT.dt.round("H"))
+                    #removing the time and keeping only the date on the Instant column
+                    df_dateonly["INSTANT"] = pd.to_datetime(df_dateonly["INSTANT"]).dt.date
+                    #gouping by instant, Integrations, by the number of occurrences of integrations
+                    new = df_dateonly.groupby(["INSTANT","ACTION"]).size()
+                    #converting the above series type variable to a dataframe
+                    df= new.to_frame().reset_index()
+                    #changing the name of the count column to "count"
+                    df = df.rename(columns= {0: "count"})
 
-                ##### PLOTTING Bar Graph ######
-                #using plotly to create a bar graph
-                print("Plotting the bar graph")
-                fig_bar = px.bar(df, x="INSTANT", y="count",
-                             hover_data=["ACTION", "count"], color="ACTION",
-                             labels={"pop":"Count"}, height=500,title="SLOW INTEGRATIONS COUNT")
+                    ##### PLOTTING Bar Graph ######
+                    #using plotly to create a bar graph
+                    print("Plotting the bar graph")
+                    fig_bar = px.bar(df, x="INSTANT", y="count",
+                                 hover_data=["ACTION", "count"], color="ACTION",
+                                 labels={"pop":"Count"}, height=500,title="SLOW INTEGRATIONS COUNT")
 
-                fig_bar.update_xaxes(type="category")
-                fig_bar.update_layout(showlegend=False,title={
-                        "y":0.9,
-                        "x":0.5,
-                        "xanchor": "center",
-                        "yanchor": "top"}
-                )
+                    fig_bar.update_xaxes(type="category")
+                    fig_bar.update_layout(showlegend=False,title={
+                            "y":0.9,
+                            "x":0.5,
+                            "xanchor": "center",
+                            "yanchor": "top"}
+                    )
 
-                fig_scatter.update_traces(dict(marker_line_width=0))
-                fig_bar.update_traces(dict(marker_line_width=0))
+                    fig_scatter.update_traces(dict(marker_line_width=0))
+                    fig_bar.update_traces(dict(marker_line_width=0))
 
-                fig_scatter.show()
-                fig_bar.show()
+                    fig_scatter.show()
+                    fig_bar.show()
 
-                print("The graphs from the Integrations logs were created. Please check your browser.")
+                    print("The graphs from the Integrations logs were created. Please check your browser.")
 
-                end = datetime.now()
-                print("\nElapsed time: {0}".format(end-start))
+                    end = datetime.now()
+                    print("\nElapsed time: {0}".format(end-start))
 
-    if not foundLogs:
-        print("The Integrations logs were not found in folder: \"" + directoryPath + "\"")
+        if not foundLogs:
+            print("The Integrations logs were not found in folder: \"" + directoryPath + "\"")
+    except Exception as e:
+        error_stack = traceback.format_exc()
+        error_log(error_stack)
+        print("A file has been created with the error message.\n\nUnexpected Error:\n", error_stack)
 
 directoryPath = input("In what folder are the Integrations logs located? ")
 create_graph(directoryPath)
